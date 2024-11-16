3aba4e32d0496b671f25d5d714a799bcca124238
CodeDevProjects <dsebert.de@gmail.com>
2024-11-16 09:33:49 +0100

    Created head.js

---
+++ head.js
@@ -0,0 +1,12 @@
+document.head.innerHTML += `
+    <meta name="language" content="de">
+    <meta name="keywords" content="nützlich, game, quiz, html, tag, css, js, python, java script, pi, raspberry, raspberry pi, computer, pc, batch, .bat, .html, .py, .js, .css, .vbs, visual basic script, terminal, cmd, command, Kommandozeile">
+    <meta name="description" content="CodeDevProjects.github.io - Lerne Programmieren und entdecke spannende Computerprojekte">
+    <meta name="robots" content="index,follow">
+    <meta name="revisit-after" content="100 days">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="home.ico" type="image/vnd.microsoft.icon">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+`;
